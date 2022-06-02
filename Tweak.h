659a0348c9b5de06a9555b60da70d1baed8bd5a1
Aric Horn <78001398+arichorn@users.noreply.github.com>
2022-06-02 18:19:31 -0500

    Create Tweak.h

---
+++ Tweak.h
@@ -0,0 +1,11 @@
+@interface YTRightNavigationButtons
+@property(readonly, nonatomic) YTQTMButton *creationButton;
+@end
+
+@interface YTNavigationButtons
+@property(readonly, nonatomic) YTQTMButton *NavigationButtons;
+@end
+
+@interface YTTopNavigationButtons
+@property(readonly, nonatomic) YTQTMButton *TopNavigationButtons;
+@end
